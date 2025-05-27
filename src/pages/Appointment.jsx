ec5a1ef77f2d6e9a099d515adb33353e548d41bc
Prince-Dwivedi2024 <dwivediprince313@gmail.com>
2025-05-27 17:34:29 +0530

    Booking slots added

--- src/pages/Appointment.jsx
+++ src/pages/Appointment.jsx
@@ -10,7 +10,12 @@ const Appointment = () => {
   //fetch workers array from AppContext
   const { workers, currencySymbol } = useContext(AppContext);
 
-  const [workInfo, setWorkInfo] = useState(null);
+  const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+
+  const [workInfo, setWorkInfo] = useState(null)
+  const [workSlots, setWorkSlots] = useState([])
+  const [slotIndex, setSlotIndex] = useState(0)
+  const [slotTime, setSlotTime] = useState('')
 
   //now find specific worker info with workId from the workers array
   const fetchWorkInfo = async () => {
@@ -18,10 +23,64 @@ const Appointment = () => {
     setWorkInfo(workInfo)
     console.log(workInfo)
   };
+
+  const getAvailableSlots = async ()=>{
+  //  first clear previous slots
+  setWorkSlots([])
+  // getting current date
+  let today = new Date()
+
+  // using this variable, calculate seven days from today
+  for(let i =0; i<7; i++){
+    // getting date with index
+    let currentDate = new Date(today)
+    currentDate.setDate(today.getDate() +i)
+
+    // setting end time of the date with index
+    let endTime = new Date()
+    endTime.setDate(today.getDate() +i)
+    endTime.setHours(21,0,0,0)
+
+    //setting hours
+    if(today.getDate() === currentDate.getDate()){ 
+       currentDate.setHours(currentDate.getHours() > 10 ? currentDate.getHours() + 1 : 10)
+       currentDate.setMinutes(currentDate.getMinutes() > 30 ? 30 : 0)
+    }
+    else{
+      currentDate.setHours(10)
+      currentDate.setMinutes(0)
+    }
+      
+    let timeSLots = []
+
+    while(currentDate < endTime){
+     let formattedTime = currentDate.toLocaleTimeString([], {hour:'2-digit', minute:'2-digit'})
+
+     //add slot to array
+     timeSLots.push({
+      datetime: new Date(currentDate),
+      time: formattedTime
+     })
+
+     //increment current time by 30 minutes
+     currentDate.setMinutes(currentDate.getMinutes() + 30)
+    }
+
+    setWorkSlots(prev => ([...prev, timeSLots]))
+  }
+  }
   //now whenever we change workId, workers array we would render and will run this function
   useEffect(() => {
     fetchWorkInfo()
   }, [workers, workId])
+
+  useEffect(() => {
+    getAvailableSlots()
+  }, [workInfo])
+
+  useEffect(() => {
+    console.log(workSlots)
+  }, [workSlots])
   
 
   return workInfo && (
@@ -46,8 +105,23 @@ const Appointment = () => {
         <p className="text-sm text-gray-500 max-w-[700px] mt-1">{workInfo.about}</p>
       </div>
       <p className="text-gray-500 font-medium mt-4">
-        Appointment fee: <span className="text-gray-600">{currencySymbol}{workInfo.fees}</span>
+        Service charge: <span className="text-gray-600">{currencySymbol}{workInfo.fees}</span>
       </p>
+    </div>
+  </div>
+
+  {/* --------------Booking slots----------- */}
+  <div className="sm:ml-72 mt-4 font-medium text-gray-700">
+    <p>Your Preferred Time</p>
+    <div className="flex gap-3 items-center w-full overflow-x-scroll mt-4">
+      {
+        workSlots.length && workSlots.map((item, index) =>(
+          <div onClick={()=> setSlotIndex(index)} className={`text-center py-6 min-w-16 rounded-full cursor-pointer ${slotIndex === index ? 'bg-primary text-white' : 'border border-gray-200'}`} key={index}>
+            <p>{item[0] && daysOfWeek[item[0].datetime.getDay()]}</p>
+            <p>{item[0] && item[0].datetime.getDate()}</p>
+          </div>
+        ))
+      }
     </div>
   </div>
   </div>
